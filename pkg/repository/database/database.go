8068b4a37d8fec909bd402c9c41cc0321f414cbc
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-08-19 19:06:57 +0530

    test

---
+++ pkg/repository/database/database.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"conference/pkg/common/utility"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() (*gorm.DB, error) {
+	config, err := utility.LoadConfig("./")
+	db, err := gorm.Open(postgres.Open(config.DSN), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+	DB = db
+	DB.AutoMigrate()
+	return db, nil
+}
